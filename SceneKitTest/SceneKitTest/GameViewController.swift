5a7bfe0b7a921e50cde32602d135b072c3b2fed5
Kaahan <kaahan15@gmail.com>
2019-07-26 10:46:38 -0500

    test1

--- SceneKitTest/SceneKitTest/GameViewController.swift
+++ SceneKitTest/SceneKitTest/GameViewController.swift
@@ -11,7 +11,12 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    
+    
+    private var counter = 0
+    private var ship:SCNNode?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,7 +35,7 @@ class GameViewController: UIViewController {
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        lightNode.position = SCNVector3(x: 0, y: 300, z: 20)
         scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
@@ -42,18 +47,23 @@ class GameViewController: UIViewController {
         
         // retrieve the ship node
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        self.ship = ship
         
         // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        //ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0, z: 0, duration: 1)))
+        // run the update function repeatedly
+        
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
+        scnView.delegate = self
         
         // set the scene to the view
         scnView.scene = scene
         
         // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
+        scnView.allowsCameraControl = false
         
         // show statistics such as fps and timing information
         scnView.showsStatistics = true
@@ -62,15 +72,15 @@ class GameViewController: UIViewController {
         scnView.backgroundColor = UIColor.black
         
         // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        //let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        //scnView.addGestureRecognizer(tapGesture)
     }
     
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
         let scnView = self.view as! SCNView
-        
+        print("tapped")
         // check what nodes are tapped
         let p = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(p, options: [:])
@@ -117,5 +127,23 @@ class GameViewController: UIViewController {
             return .all
         }
     }
+    
+    func update() {
+        print("updating")
+        
+    }
+}
 
+// 1
+extension GameViewController: SCNSceneRendererDelegate {
+    // 2
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        // 3
+        print(counter)
+        counter += 1
+        
+        if let spaceShip = ship {
+            spaceShip.position = SCNVector3(spaceShip.position.x, spaceShip.position.y, spaceShip.position.z-1)
+        }
+    }
 }
